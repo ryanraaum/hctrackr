eb03d647be0e7d507e74befe3763bc9749fef54d
Ryan Raaum <github@raaum.org>
2025-06-30 20:48:17 -0400

    converting to target terminology

--- tests/testthat/test-point-tagger.R
+++ tests/testthat/test-point-tagger.R
@@ -4,19 +4,20 @@ kaaterskill <- tracktools::kaaterskill
 huckleberry <- tracktools::huckleberry
 sherrillnd <- tracktools::sherrillnd
 
-test_that("make_point_tagger_function works", {
-  point_tagger <- expect_no_error(make_point_tagger_function(peaks, "peak_id"))
-  expect_true(is.function(point_tagger))
+test_that("make_target_tagger_function works", {
+  target_tagger <- expect_no_error(make_target_tagger_function(peaks, "peak_id"))
+  expect_true(is.function(target_tagger))
 
-  kaaterskill_peaks <- expect_no_error(point_tagger(kaaterskill$tracks[[1]]))
+  kaaterskill_peaks <- expect_no_error(target_tagger(kaaterskill$tracks[[1]]))
   expect_true(nrow(kaaterskill_peaks) == 1)
-  expect_true("Kaaterskill High Peak" %in% kaaterskill_peaks$point_id)
+  expect_true("Kaaterskill High Peak" %in% kaaterskill_peaks$target_id)
 
-  huckleberry_peaks <- expect_no_error(point_tagger(huckleberry$tracks[[1]]))
+  huckleberry_peaks <- expect_no_error(target_tagger(huckleberry$tracks[[1]]))
   expect_true(is.null(huckleberry_peaks))
 
-  sherrillnd_peaks <- expect_no_error(point_tagger(sherrillnd$tracks[[1]]))
+  sherrillnd_peaks <- expect_no_error(target_tagger(sherrillnd$tracks[[1]]))
   expect_true(nrow(sherrillnd_peaks) == 2)
-  expect_true("Sherrill" %in% sherrillnd_peaks$point_id)
-  expect_true("North Dome" %in% sherrillnd_peaks$point_id)
+  expect_true("Sherrill" %in% sherrillnd_peaks$target_id)
+  expect_true("North Dome" %in% sherrillnd_peaks$target_id)
 })
+
